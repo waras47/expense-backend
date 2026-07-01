e4a72ab10a072618eee1f8f2e3bfaa74ca27e9ad
Listantiyo <freelanceme779@gmail.com>
2026-07-01 23:15:47 +0700

    FEAT:: debt implement full debt usecase logic
    Add full repository interactions with error handling for all debt CRUD operations. Implement pagination validation, default values and 100 max limit for the GetAll method. Add existence checks before update and delete, and implement data merging for update calls. Also add a TODO comment for future account ID parameter support.

--- internal/usecase/debt_usecase.go
+++ internal/usecase/debt_usecase.go
@@ -14,21 +14,76 @@ func NewDebtUsecase(repo domain.DebtRepository) domain.DebtUsecase {
 }
 
 func (uc *debtUsecase) Get(ctx context.Context, id int64) (*domain.Debt, error) {
-	return nil, nil
+	debt, err := uc.repo.FindByID(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	return debt, nil
 }
 
+// TODO: tambahkan parameter id akun jika ada akun
 func (uc *debtUsecase) GetAll(ctx context.Context, page, limit int64) ([]domain.Debt, int64, error) {
-	return nil, 0, nil
+	// Validation minimal
+	if page < 1 {
+		page = 1
+	}
+	if limit < 1 {
+		limit = 10
+	}
+
+	maxLimit := int64(100)
+	if limit > maxLimit {
+		limit = maxLimit
+	}
+	offset := (page - 1) * limit
+
+	// Process
+	debts, err := uc.repo.FindAll(ctx, limit, offset)
+	if err != nil {
+		return []domain.Debt{}, 0, err
+	}
+
+	countAll := uc.repo.CountAll(ctx)
+
+	return debts, countAll, nil
 }
 
 func (uc *debtUsecase) Create(ctx context.Context, input *domain.Debt) (*domain.Debt, error) {
-	return nil, nil
+	/* Validation payload handled by gin with validation/v10 */
+	debt, err := uc.repo.Create(ctx, input)
+	if err != nil {
+		return nil, err
+	}
+
+	return debt, nil
 }
 
 func (uc *debtUsecase) Update(ctx context.Context, id int64, input *domain.Debt) error {
+	// Curent update is replacing with new data.
+	debt, err := uc.repo.FindByID(ctx, id)
+	if err != nil {
+		return err
+	}
+
+	// Merge existing debt data with new data from payload then do Update
+	if err := uc.repo.Update(ctx, debt.MergeWithNewData(input)); err != nil {
+		return err
+	}
+
 	return nil
 }
 
+// Soft Delete
 func (uc *debtUsecase) Delete(ctx context.Context, id int64) error {
+	// Validate data exists
+	if _, err := uc.repo.FindByID(ctx, id); err != nil {
+		return err
+	}
+
+	if err := uc.repo.Delete(ctx, id); err != nil {
+		return err
+	}
+
 	return nil
 }
